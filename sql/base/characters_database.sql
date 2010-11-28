6da46679db5ede48efce57ca305f90bef2992ecc
Arktus <arktus@cactusemu.com>
2010-11-28 13:13:14 +0100

    Updates from Trinity Core. Now CactusEMU is based on commite8847394b622fd6cf0712b60fb87cf9501698677 from Trinity Core. http://code.google.com/p/trinitycore/source/detail?r=e8847394b622fd6cf0712b60fb87cf9501698677
    Former-commit-id: adf30c9f3119ff0a748d676cb74bf8537282fce2

--- sql/base/characters_database.sql
+++ sql/base/characters_database.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e946be68ca811d0c9337cba45154718b8f30592864f2fc3563af49b11486a789
-size 71609
+oid sha256:37e9752ec883bfa87117dcd3d0bafc29b8fd3267763b8b14eeae299734444ac0
+size 71670
