566dcddf0678ecce1ffc7d03957da9f98527b3a9
Kierkegaard <kierkegaard69@gmail.com>
2010-12-14 21:39:47 +0100

    [57] Import some TrinityCore fixes. Patch by Keany.
    Signed-off-by: Kierkegaard <kierkegaard69@gmail.com>
    
    Former-commit-id: 68e1b6c447a6917cff8aa551e4190afdb61b4968

--- sql/base/characters_database.sql
+++ sql/base/characters_database.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ebcb46631c60ce40d759909adbabbb312d482530eff013dea7368e95178fd0af
-size 65805
+oid sha256:c16e7995ac9e7ff4dfdf77b8755641dac491deca12918d136043ba75fa9c8e0d
+size 66687
--- sql/base/world_database.sql
+++ sql/base/world_database.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0820a6a989758c78e3d1dc896c71ecc9c638894e3bb20922a03b60235ae1c6df
-size 990551
+oid sha256:43ad46deb480c6a43b5c69e87c419f5ccf819edf3b17190a9c58c3bd5d8cc2f5
+size 991134
