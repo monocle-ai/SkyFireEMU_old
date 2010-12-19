7388476245fa0062f071925a2da7165e8c494f35
Silinoron <silinoron@gmail.com>
2010-12-19 11:18:25 -0800

    Fix quest template table at Synric's request.
    Former-commit-id: f52bd79ec02e8cb6d9a440a1dcf70ab04f4cf1a3

--- sql/base/world_database.sql
+++ sql/base/world_database.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:43ad46deb480c6a43b5c69e87c419f5ccf819edf3b17190a9c58c3bd5d8cc2f5
-size 991134
+oid sha256:c5545bed8e13dbe9d2e65c8395709f6731a62617b19743341e5f22403991601b
+size 992674
---
+++ sql/updates/2010_12_19_world_quest_template.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:9ac8bad0ecf2fd2c667b0d03f9b7cc814536c87957a40eafb76121623167599b
+size 3091
