2e1a418980ce79dd897fa6f65840cea5ae152a97
Silinoron <silinoron@gmail.com>
2010-12-19 11:29:02 -0800

    Add in a forgotten field.
    Former-commit-id: 66b191693fe5707f71d6d869f83eca557450c8fd

--- sql/updates/2010_12_19_world_quest_template.sql
+++ sql/updates/2010_12_19_world_quest_template.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9ac8bad0ecf2fd2c667b0d03f9b7cc814536c87957a40eafb76121623167599b
-size 3091
+oid sha256:e67c322800962432fb9eed506539f7995a8cb8337234b369c9eec35c99730768
+size 3200
