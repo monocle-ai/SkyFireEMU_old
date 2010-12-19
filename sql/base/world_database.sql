401addf2fe9d1dcb29e2df0b2e39835043919324
Silinoron <silinoron@gmail.com>
2010-12-19 12:36:39 -0800

    Fix a typo. Thanks nowhell.
    Former-commit-id: 5bca3733fd18c9d513699c95b866384014f9dfa5

--- sql/base/world_database.sql
+++ sql/base/world_database.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f6d37ca4fbc2c8492e162097259099cc07f81bbf53b85e644344b302f3aee700
-size 992731
+oid sha256:4f9daaafc33330aeffeee068640528d9b655ccd613644c120f94b451288e4e31
+size 992732
