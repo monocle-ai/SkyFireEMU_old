3ec11b54fcc31b84ea3284cbbcc16e523cf6d88d
Arktus <arktus@cactusemu.com>
2010-12-26 23:06:41 +0100

    Fix pet system. You can now have 5 different pet if you have a hunter. This WILL break something :-) TODO: * pet stable. * pet stat.
    Merry Christmas!
    
    
    Former-commit-id: 9155faf3ceff1b486847bbfe932868f59c4b1cb0

--- sql/base/characters_database.sql
+++ sql/base/characters_database.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c16e7995ac9e7ff4dfdf77b8755641dac491deca12918d136043ba75fa9c8e0d
-size 66687
+oid sha256:e9cdf9382c00da4627e5c58757453d6c8ff8db75553436938157a38c328b44ca
+size 66766
