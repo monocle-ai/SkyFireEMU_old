eaee5e5ff0955dbcb09ef270b653f90c0a7243a7
Kierkegaard <kierkegaard69@gmail.com>
2010-12-29 16:21:53 +0100

    Fix and cleanup characters_database.sql
    Signed-off-by: Kierkegaard <kierkegaard69@gmail.com>
    
    Former-commit-id: a475fa2a6a4f267b0a2c0aa7df1c6673bd224d93

--- sql/base/characters_database.sql
+++ sql/base/characters_database.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c16e7995ac9e7ff4dfdf77b8755641dac491deca12918d136043ba75fa9c8e0d
-size 66687
+oid sha256:5b8ae065067f1a9f2fc3bcba66439cb4fa54089f930eda89535807b7981b7710
+size 76225
