4d8982830ee12a999a6d4c09115c169d8bcac555
SkyFire <nonya@effing_beeezwax.gtfo>
2011-02-08 15:23:10 -0600

    Few 406 changes. Still lots to get working. Will not let you in yet.
    Signed-off-by: SkyFire <nonya@effing_beeezwax.gtfo>
    
    Former-commit-id: b3b29254d0d3117b0da35ca432845a808190b71c

--- sql/base/auth_database.sql
+++ sql/base/auth_database.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1e702ae7c9230ebf705194c0c53b5280a66060979f69a4fba03fe2e647ab90fb
+oid sha256:f09131f27adcca41f5a89140eec20f44ddbecfa63caa0fb17fff45ac432ffa5e
 size 7971
