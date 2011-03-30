d52c1a14893b8dd22035fbf24305e6dea09c8850
AlterEgo <admin@projectskyfire.org>
2011-03-30 07:58:29 -0500

    Cleaned up the base character database. This is for fresh installs only.
    Signed-off-by: AlterEgo <admin@projectskyfire.org>
    
    Former-commit-id: d23ed41dfa7e015dfa263f1b29c0f3d928f1d0f0

--- sql/base/characters_database.sql
+++ sql/base/characters_database.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9aa28da74f2c641ab70cf480c2401efb7e56b726800e0f0b53e0e4cdf575eff9
-size 76255
+oid sha256:30022df9b114e4839cc16d9baf42db8545746e0d465ff4d14c88bcddc5e27bb3
+size 56544
