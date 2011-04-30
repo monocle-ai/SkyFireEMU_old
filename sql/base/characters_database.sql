b7c707e5de919b86d298934b4e3c0108146ad1e3
Cerey <thyros82@gmx.de>
2011-04-30 13:52:30 +0200

    [884] Core/QuestStatus: - Separate rewarded quests from active quests, and store them in a new table to reduce database size - Drop the no longer needed `rewarded` column from character_queststatus for smaller table size - Prevent filling the database with dropped quests   - Delete useless records - Implement queststatus save "queues" instead of states - Minor optimizations Core/Spells: Add dummy effect for Saronite Vapors (63322) (Used in General Vezax encounter) Based on original code by Lopin Fix EffectQuestClear, also clear rewarded quests again Log abandoned quests (requested, can be used in statistics) Split RemoveQuest into 2 separate functions Core/Spells: Fix SPELL_EFFECT_FORCE_CAST(140), SPELL_EFFECT_FORCE_CAST_WITH_VALUE(141). These should force the target to cast a certain spell, not serve as a proxy for the original caster to cast another spell. Core/CharacterDatabaseCleaner: Cleanup Core/CharacterDatabaseCleaner: Implement PersistentCharacterCleanFlags config value which can prevent specific cleaning flags from being disabled after a cleanup Core/CharacterDatabaseCleaner: Store character cleaning flags in World for runtime usage Core/Player: Keep and store abandoned quests in the DB if CLEANING_FLAG_QUESTSTATUS is not set Core/QuestStatus: Prevent inactive quest from being added into the questlog Update character db. Core/Vehicles: - Prevent client ejecting passengers from non-ejectable seats. - Chart some more VehicleSeatFlags and VehicleSeatFlagsB. Research by linencloth Core/Vehicles: - Redefine VEHICLE_SEAT_FLAG_USABLE as VEHICLE_SEAT_FLAG_CAN_ENTER_OR_EXIT. This flag determines whether or not the player has control over entering/exiting a vehicleseat. Thanks to linencloth for help with research.
    - Implement VEHICLE_SEAT_FLAG_CAN_ENTER_OR_EXIT exit restriction
    - Remove MOVEMENTFLAG_ROOT on vehicle exit
    Core/Handlers: Allow gamemasters to send mails with customer support badge depending on gm mode (.gm off/on)
    Core/Vehicles: Store guids in vehicleseat structures instead of pointers. Fixes a crash with vendor-carrying Mammoth mounts.
    Scripts: Implemented virtual function DoCastSpellOnPlayers (self-explanatory) and CheckRequiredBosses checking states of other bosses before allowing combat with selected one
    
    Scripts/Icecrown Citadel: Added checks to prevent skipping bosses
    Core/Auras: Fixed transform aura stacking logic
    
    Apply missing rev numbers from last commits.
    
    patch by linencloth / Shauren / Machiavelli / leak.
    
    
    Former-commit-id: 8537ab0f8e41816091fa278209fb7099f09d2edb

--- sql/base/characters_database.sql
+++ sql/base/characters_database.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c12b77d2eda4635d25f18a99a5c07010826cb1b5c7c7ad8720a569338b558231
-size 56563
+oid sha256:4aa2c93766cd1127c0ff3825f698781d69a643f14aaf141d5e7b441c48ac65f5
+size 58827
