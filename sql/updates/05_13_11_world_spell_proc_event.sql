713e858303f808e31faa65417898535bb2878f97
ZenitH <alex.c2312@yahoo.com>
2011-05-13 11:48:29 +0300

    Fixed last SQL ( less QQ now )
    Former-commit-id: 40a3b970b3649d6c60bd6a997c830d3954812d2d

--- sql/updates/05_13_11_world_spell_proc_event.sql
+++ sql/updates/05_13_11_world_spell_proc_event.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:148e43ddbf8dbd45727e33d137a90382edee7d52cc7a63b94fe1a8f37048c4d6
-size 4173
+oid sha256:4b2c4920b2486c671c96c43274b53f8c3553bd99d905cf26db88e6ae3e936130
+size 4136
