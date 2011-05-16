649493a4a3d3ff7af6540b97a8512f6ec9266400
Skyfire <admin@projectskyfire.org>
2011-05-16 07:50:49 -0500

    [1180] Removed applied updates.
    Signed-off-by: Skyfire <admin@projectskyfire.org>
    
    Former-commit-id: 57fa2b0bd9b2b9b1cf11e6f633b358d8cc9b17dc

--- database/updates/05_01_11_world_spell_proc_event.sql
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:14ba0a349c8a02b6492518a44cdffc0d8d33779868a989925af95b53f8f2197d
-size 2482
--- database/updates/05_04_11_world_uldaman.sql
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:96b7d0715a2fb2d3c06a38523641fb65c00d1dfaeab5af580732773d480b5797
-size 4828
