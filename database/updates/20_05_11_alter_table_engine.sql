0013d898213ff723f14d5cb048aac22d36befbf7
ZenitH <alex.c2312@yahoo.com>
2011-05-20 21:29:33 +0300

    SQL: Reworked previous SQL to avoid confusion
    Former-commit-id: 56f932c62d0183f7fe90a05a58229e4c3fa3f70b

--- database/updates/20_05_11_alter_table_engine.sql
+++ database/updates/20_05_11_alter_table_engine.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b1471fa11363965dea50560c93f8d46893fa3d9d01c4f3778d8f64bc8b31e947
-size 1635
+oid sha256:9b7137c2fdf341e338d5d7a7e93cbe3a2127836717bc07e72e4d58a5dcfa1b60
+size 1525
