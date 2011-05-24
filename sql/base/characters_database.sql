3e29be40d35a8153703333f86dd1397d06a55403
Snajl <snajl@project-nemesis.cz>
2011-05-24 10:25:23 +0200

    OMG, bad dump, again Updated BASE SQL
    Former-commit-id: 39f883dc63da8b85f57eb0eaa9df37a4dc06cf3e

--- sql/base/characters_database.sql
+++ sql/base/characters_database.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:63ba67e2d4596e27da6a4ec714acb0b906debbbce9c737dc42fd1b1434f2a2a7
-size 50342
+oid sha256:b7012a5ec98a5b01a7562d7099afaccba96c5e79341c05f7319863a2c568de4e
+size 61762
