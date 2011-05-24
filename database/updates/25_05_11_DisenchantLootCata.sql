e9be840b83c32470f05ad68e66c75f9cdee9d526
AriDEV <ari@nightandmagic.cz>
2011-05-24 16:20:07 -0700

    DB/Syntax: UPDATED 1 More Syntax Error
    Former-commit-id: 25f703c512af2effb22d3c7f4f48672e88a953c1

--- database/updates/25_05_11_DisenchantLootCata.sql
+++ database/updates/25_05_11_DisenchantLootCata.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:07d660ef40a3b00eaa90efdeb61c2092522932c03d6e5912cf3ef411a1613d3a
+oid sha256:170c6f8faea79c267aa7379c82e919f74eb16e2768b14a12f459f825ef7754dc
 size 930
