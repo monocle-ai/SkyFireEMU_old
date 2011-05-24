e9c81d6bc9b3df00b80090a0de30a3190907e45c
Snajl <snajl@project-nemesis.cz>
2011-05-24 11:39:11 +0200

    Finish, fixed login problem - crash, Updated SQL update, Arena Load
    Former-commit-id: e61a01f7cffa634f61b2ae14784e821b6b9b0718

--- sql/updates/character/24_05_11_characters_characters.sql
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:08c38c7f3332ca07c2aad59e194ac9f87a9e7bfb83e7539fbaead6b1db6732f7
-size 3448
---
+++ sql/updates/character/24_05_11_characters_misc.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:e8794fef7c2938904a15e3d30d23af9e49e6f8f1e5ab97a37a56695e71370969
+size 3970
