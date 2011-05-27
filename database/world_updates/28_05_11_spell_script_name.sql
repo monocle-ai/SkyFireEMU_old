00697221dcdc5c92e6db0e4aeb29ea749b6241ec
Rad3k <rad3k.t@seznam.cz>
2011-05-28 01:20:57 +0200

    Fixed last commit
    Former-commit-id: 45eb4f17d2e7af530adb43e2035c04537a5e0f79

--- database/world_updates/28_05_11_spell_script_name.sql
+++ database/world_updates/28_05_11_spell_script_name.sql
@@ -1,3 +1,2 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:2c4ed03368fa8cd0f5aea87ac9c15199a35b2cc790f40e929fff8eb823cbdbb9
-size 1518
+DELETE FROM `spell_script_names` WHERE `spell_id` = -48721;
+INSERT INTO `spell_script_names` VALUES (-48721,'spell_dk_blood_boil');
