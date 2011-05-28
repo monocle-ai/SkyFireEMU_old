d3a4693ee2c8e7572196f6d42e87c8db43c7913a
AriDEV <michalik.andrej@centrum.sk>
2011-05-28 23:20:14 +0200

    DB/FORMAT: YY_MM_DD__DAYREV_DATABASE_TABLE.sql DB/SYNTAX: ``````  <--- WTF ? USE STANDART WITHOU IT PLS
    Signed-off-by: AriDEV <michalik.andrej@centrum.sk>
    
    Former-commit-id: 3e9de2dbcf8b10e6eb4394a25dd52bc44732d883

---
+++ database/world_updates/11_05_28__01_world_creature_template.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:2d7118dc1c0b16b3be8c47190d0a7ed0e985b9500ac81f01a04cac564bdf2c4f
+size 1511
--- database/world_updates/28_05_11_creature_template_tolvir.sql
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:22726cae42a6c5ba4dda2ca13106478cc7546019569229b8500c4179e4978963
-size 1623
