08ef6a655542b92414337ebcd201eb13634c79b6
Snajl <snajl@project-nemesis.cz>
2011-05-29 21:50:28 +0200

    DB/Updates: Fix typo in sql update for pets
    Former-commit-id: a3bdf66353b9704851e8ed5184149e547fd784ce

--- sql/updates/character/11_05_29__01_characters_character_pet.sql
+++ sql/updates/character/11_05_29__01_characters_character_pet.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e06467db0889c88956a60cf4648b8a2e92a71c64a82b0d2b86a78bd2df4ab9cc
-size 1119
+oid sha256:3bbc2a7b432f90b71a20f8aa6581c944547477f0a38687188febe8a2bebc95ad
+size 1082
