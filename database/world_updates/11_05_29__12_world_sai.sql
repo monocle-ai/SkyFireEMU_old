f686bc2077243012d3e5d964ddfc900e2fc2cf87
cooler-SAI <cooler19860212@yahoo.com>
2011-05-30 00:05:36 +0300

    DB/SA: fixed Serfex the Reaver entry, thanks to Starzu
    Former-commit-id: 1c21a4b04921b2487c5a1865f6870434e516614f

--- database/world_updates/11_05_29__12_world_sai.sql
+++ database/world_updates/11_05_29__12_world_sai.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5d7a99e64f3427c9456b1e67f6c0412eaf7110b19beb0bf9658827f7fbd688a5
+oid sha256:34304ca79ad1b173e69d5085cd77055c15ffbbc39b31a598d3baed6e7d855751
 size 70106
