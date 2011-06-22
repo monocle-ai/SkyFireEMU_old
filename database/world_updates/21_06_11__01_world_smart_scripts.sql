36342700d7bb2a8cb5ebb036f14adc094b192da2
cooler-SAI <cooler19860212@yahoo.com>
2011-06-22 19:48:13 +0300

    DB/SAI: typo fix. thanks to Aleksandru
    Former-commit-id: c919e71c3f0b304da6b19f3aee1eb280a2a1d014

--- database/world_updates/21_06_11__01_world_smart_scripts.sql
+++ database/world_updates/21_06_11__01_world_smart_scripts.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:98170a8e1554df952eb129a3c2525504fb88ce4be7e8bfb4004a2bba7c1bdcbf
-size 4750
+oid sha256:3a3ef0e1eb8e70ad1cede74e3f42a15f5c95ece012b82bcc5d70cc9c604f37fe
+size 4754
