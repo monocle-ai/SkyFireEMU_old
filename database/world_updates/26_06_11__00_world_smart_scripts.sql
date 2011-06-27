bdeaab16317a3869dab9da13b94abb1814409a67
cooler-SAI <cooler19860212@yahoo.com>
2011-06-27 09:40:43 +0300

    DB/SAI: typo fix for Navicat users xD
    Former-commit-id: df9d0d6210d4dc5a56e462eb0dbd2986c5bb9cf4

--- database/world_updates/26_06_11__00_world_smart_scripts.sql
+++ database/world_updates/26_06_11__00_world_smart_scripts.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:09bd55f6c3ae75ee7c38d7d00d0ab3c9cbf0ab687587bf5a5c220ba177ef14e3
-size 135552
+oid sha256:0251d5cbfb356f5ce30d83f0c65f7420f2fa381a1afbe41fb252491a63fcdd62
+size 135553
