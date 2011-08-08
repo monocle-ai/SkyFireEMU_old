ae16abc4bee5307245395a2a638656677a9927f0
Bootz <Stage6Dev@EMPulsegaming.com>
2011-08-08 14:17:30 -0500

    SQL: Resync characters db base SQL with rollups and redump with recent mysqldump version in preparation for char db storage type cleanup
    Signed-off-by: Bootz <Stage6Dev@EMPulsegaming.com>
    
    Former-commit-id: 0e269bffac6bcf2bd9e3a10db544cf727e3a2333

--- sql/base/character/character.sql
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:a78f330947e17ec9bc7eefd90d8ee3a439083e40fa45261b0ac206448165f5f0
-size 63770
---
+++ sql/base/character/characters_database.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:e15f2e9daf7242e58097ed8553b4c2cb6b6328a7ad3b4804169a9769ed0345d4
+size 80603
