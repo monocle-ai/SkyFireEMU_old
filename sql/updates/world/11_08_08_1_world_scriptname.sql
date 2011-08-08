665656b8cb88bbed1e781dc7a1e06c8c73d1ae8a
Bootz <Stage6Dev@EMPulsegaming.com>
2011-08-08 11:25:20 -0500

    Scripts: full directory clean-up
    tabs to spaces
    removed extra line returns
    cleaned up line ending
    cleaned up trailing white spaces...
    -- needed done, for preping for the scripting updates, and to keeping source clean.
    -- also added what was missing from previous commit...
    
    
    Former-commit-id: b217f4053239ec6b80c55a4d73df95ba0efe3be4

---
+++ sql/updates/world/11_08_08_1_world_scriptname.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:58a9445b3d0f04fe9c4ff181c9778d8de828aa4da009cbd8beacf789767864a4
+size 3151
---
+++ sql/updates/world/11_08_08_2_world_scriptname.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:f4bbe918710e0a2a49d7a0acc7536e95d1920855f1cee43a3b7fadbab5c7ff54
+size 742
