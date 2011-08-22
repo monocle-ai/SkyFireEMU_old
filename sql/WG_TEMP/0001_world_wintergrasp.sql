bc44296d74c4a9d05d90756263b4aabb3c4e0401
Bootz <Stage6Dev@EMPulseGaming.com>
2011-08-21 21:04:42 -0500

    SQL: replaced trinity_string -> skyfire_string...sql imports correctly now
    Signed-off-by: Bootz <Stage6Dev@EMPulseGaming.com>
    
    Former-commit-id: 807475771c9540273f220b7730bf7b02c6621793

--- sql/WG_TEMP/0001_world_wintergrasp.sql
+++ sql/WG_TEMP/0001_world_wintergrasp.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:170f07f6ad73554ba11fb26a0737b786e1045349afa3643be2d121745e8c7b43
+oid sha256:d6a8cd7895b14646f11fd969b1b9a1dde6639d451700ef75cb1edd7d130b2e2d
 size 12581
