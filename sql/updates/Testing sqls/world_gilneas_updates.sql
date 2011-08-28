1b6dca706416b20a55fc4cc78a10e6fbba2baf52
Bootz <Stage6Dev@EMPulseGaming.com>
2011-08-28 03:15:39 -0500

    Scripts/Gilneas: added quest fixes for 14293, and 14154.
    Scripts/Gilneas: coding standards and clen-up done.
    
    this clean-up and change of the way it was coded was to shut down any QQ that we've had in past commits...
    
    sry it was committed this way... but i wanted to show that what was said to these select few in comments "WAS NOT" an open/empty threat, on my part.
    
    
    Former-commit-id: d3ee56f59c3704bce35ad8d680ab5e580b277d98

--- sql/updates/Testing sqls/world_gilneas_updates.sql
+++ sql/updates/Testing sqls/world_gilneas_updates.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8cdc7821da9069947471a99895f444acb25fbef0a268b2f31b51b381271a91ca
-size 47429
+oid sha256:d81de1424e88dd39632ac1ffa8ff53f415d265a728ad484e24a5eafddae35784
+size 48253
