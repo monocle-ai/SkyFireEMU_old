0cd33e39d95a7948f8a3751b3a125751047c8e50
Bootz <Stage6Dev@EMPulseGaming.com>
2011-08-28 02:36:29 -0500

    Scripts/Gilneas: implemented Gilneas scripting, for more community work on the zone.
    all credits for this scripting goes to: SaiFi0102 and Kupker
    
    
    Former-commit-id: 09b6e3ac10e276b33631fb0c0267667215c611f4

---
+++ sql/updates/Testing sqls/world_gilneas.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:e38ffccf5f0c08598ae4aeaf802e4a243277ecb1909a85b2ed5cbb737eebe04e
+size 89750
---
+++ sql/updates/Testing sqls/world_gilneas_updates.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:8cdc7821da9069947471a99895f444acb25fbef0a268b2f31b51b381271a91ca
+size 47429
