88307f2b26badf651464bd25c254b14d91c1c5cf
et2012 <et2009@gmail.com>
2011-09-30 15:11:44 +0800

    Misc: fix reforge gossip menu option icon
    Former-commit-id: 092d1326676dbbcd09a39eeea11aab292fb58e2c

--- sql/updates/world/11_09_30_0_world_reforge_misc.sql
+++ sql/updates/world/11_09_30_0_world_reforge_misc.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:29a69dbcb90e7ca1a7cac2bbeb0eaad2235b31189c7dce6437368a0ac67d4a16
+oid sha256:6767465f0f0c1dfa34a571680ae0b5db1cd8555065975dd58f0d394d028d80c9
 size 652
